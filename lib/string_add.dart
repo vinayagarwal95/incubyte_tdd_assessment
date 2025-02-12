f515965eae05cd8423e8a7a54e38f85d47537afe
Vinay PC <52975722+vinayagarwal95@users.noreply.github.com>
2025-02-12 16:34:13 +0530

    enhancement- default delimiter added

--- lib/string_add.dart
+++ lib/string_add.dart
@@ -4,4 +4,28 @@ int add(String numbers) {
     return 0;
   }
 
+  // Default delimiters are comma and newline.
+  List<String> delimiters = [",", "\n"];
+  String numbersPart = numbers;
+
+  // Check if a custom delimiter is specified.
+  if (numbers.startsWith("//")) {
+    // Find the end of the delimiter definition (first newline).
+    int newlineIndex = numbers.indexOf("\n");
+    if (newlineIndex != -1) {
+      // Extract the delimiter definition.
+      String delimiterSpec = numbers.substring(2, newlineIndex);
+      
+      // If the delimiter is wrapped in square brackets, remove them.
+      if (delimiterSpec.startsWith("[") && delimiterSpec.endsWith("]")) {
+        delimiterSpec = delimiterSpec.substring(1, delimiterSpec.length - 1);
+      }
+      
+      // Use the specified delimiter.
+      delimiters = [delimiterSpec];
+      // The rest of the string contains the numbers.
+      numbersPart = numbers.substring(newlineIndex + 1);
+    }
+  }
+
 }
